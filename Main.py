cf16198921ec74e417e64238b2e7531db2e6d8e5
AbrilPal <abrilpal99@gmail.com>
2023-02-22 22:30:16 -0600

    construccion del AFN

--- Main.py
+++ Main.py
@@ -8,9 +8,12 @@
 
 from Arbol import *
 from Infix_a_Postfix import *
+from AFN import *
 
 expresion_regular = input("Ingrese la expresion regular: ")
 
 expresion_postfix = Infix_Postfix(expresion_regular)
 tree = construir_arbol(expresion_postfix)
 imprimir_arbol(tree, "arbol_sintactico")
+afn = construir_AFN(tree)
+print(afn)
